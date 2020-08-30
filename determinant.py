4d5028ba0c4ccfaa74526c8fa6f51d3a158c1a0a
Phillibob55 <itsluqman55@gmail.com>
2020-08-30 11:08:53 +0500

    uNDER pROGRESS

--- determinant.py
+++ determinant.py
@@ -1,14 +1,15 @@
 def cofactors(matrix):
     cofactor = [[0,0],[0,0]]
-    for x in range(3):
-        for y in range(3):
-            pass
+    for x in range(2):
+        for y in range(2):
+            cofactor[x][y] = 1
+    return cofactor
 
 def determinant(matrix):
     det = matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
     return det
 
-matrix = [[1,2],[3,4]]
-det = determinant(matrix)
-cofactors(matrix)
-print(det)
+matrix = [[1,4,7],[2,5,8],[3,6,9]]
+#det = determinant(matrix)
+print(cofactors(matrix))
+#print(det)
