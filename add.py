56adf41518156ac25de4bd06b7712caab83643d8
Phillibob55 <itsluqman55@gmail.com>
2020-09-24 18:36:42 +0500

    Matrices
    Trying to create a matrix datatyoe attempt
    DAY 1

--- add.py
+++ add.py
@@ -2,7 +2,6 @@ matrix1 = [[1,2,3],[4,5,6],[7,8,9]]
 matrix2 = [[9,8,7],[6,5,4],[3,2,1]]
 
 matrix3 = matrix1
-order = len(matrix1)
 i = j = 0
 while (i < 3):
     j = 0
---
+++ matrix.py
@@ -0,0 +1,126 @@
+class Matrix:
+    def __init__(self, m , n):
+        self.m = m
+        self.n = n
+        self.create()
+    
+    @classmethod
+    def create(cls):
+        values_for_matrix = input("Give values for the matrix in the format, <row 1 > <row 2> <row 3>..... Use spaces for separation. e.g values for a matrix with 3 x 3 order: 1 2 3 4 5 6 7 8 9:" )
+        values_for_matrix = values_for_matrix.split()
+        print(values_for_matrix)
+        for x in values_for_matrix:
+            for y in range(0,m):
+                for z in range(0,n):
+                    # From the values in values_for_matrices, make a list of size [m][n]
+                    pass
+
+    @classmethod
+    def add(cls, mat1, mat2):
+        mat1_size = Matrix.contents(mat1)
+        mat2_size = Matrix.contents(mat2)
+        
+        if (mat1_size == mat2_size) and (len(mat1) == len(mat2)):
+            # add matrices mat1 and mat2
+            rows = len(mat1)
+            columns = mat1_size/rows
+            mat3 = mat1
+            i = j = 0
+            while (i < rows):
+                j = 0
+                while(j < columns):
+                    mat3[i][j] = mat1[i][j] + mat2[i][j]
+                    j+=1
+                i+=1
+            return mat3
+        else:
+            print("Matrices donot have the same order!")
+
+    @classmethod
+    def subtract(cls, mat1, mat2):
+        mat1_size = Matrix.contents(mat1)
+        mat2_size = Matrix.contents(mat2)
+        
+        if (mat1_size == mat2_size) and (len(mat1) == len(mat2)):
+            rows = len(mat1)
+            columns = mat1_size/rows
+            mat3 = mat1
+            i = j = 0
+
+            while (i < rows):
+                j = 0
+                while(j < columns):
+                    mat3[i][j] = mat1[i][j] + mat2[i][j]
+                    j+=1
+                i+=1
+            return mat3
+        else:
+            print("Matrices donot have the same order!")
+
+    @classmethod
+    def contents(cls, mat):
+        count = 0
+        for x in len(mat):
+            count += len(x)
+        return count
+
+    @classmethod
+    def output(cls, mat):
+        for x in mat:
+            print(x)
+
+    @classmethod
+    def multiply(cls, mat1, mat2):
+        if isinstance(mat1,Matrix) and isinstance(mat2,Matrix):
+            return Matrix.matrix_product(mat1, mat2)
+
+        elif isinstance(mat1,Matrix) or isinstance(mat2,Matrix):
+            return Matrix.scalar_product(mat1,mat2)
+
+    @classmethod
+    def scalar_product(cls, mat1, mat2):
+        pass
+    
+    @classmethod
+    def matrix_product(cls, mat1, mat2):
+        pass
+
+    @classmethod
+    def transpose(cls, mat):
+        pass
+    
+    @classmethod
+    def determinant(cls, mat):
+        pass
+
+    # ***** Adjoint before determinant*****
+
+    @classmethod
+    def adjoint(cls, mat):
+        pass
+
+    @classmethod
+    def inverse(cls, mat):
+        pass
+
+
+if __name__ == "__main__":
+
+    # declare a matrix in the form of [[row 1],[row 2],[row 3]]
+    # m = number of rows, n = number of rows.
+    # Accept values of x and y e.g 
+    # Enter the order of the matrix in this format => rows coloumn: 2 3
+    try:
+        order = input("Enter the order of the matrix in this format => rows coloumn:")
+        order_list = order.split()
+        n = int(order_list.pop())
+        m = int(order_list.pop())
+    except:
+        print("Improper input!")
+        exit()
+
+    if(len(order_list) != 0):
+        print("Improper Input!")
+        exit()
+    matrix1 = Matrix(m,n)
+    pass
